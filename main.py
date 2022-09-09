4c0682f7a1842b3ac5de028ea5a403d5c093e6eb
lmy375 <lmy375@139.com>
2022-09-09 10:13:01 +0800

    Add main.py.(fix)

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from peth.__main__ import main
 
 # Set output to current folder.
 config.OUTPUT_PATH = os.path.join(os.path.dirname(__file__), 'output')
+config.CACHE_PATH = os.path.join(config.OUTPUT_PATH, 'contract_info_cache')
+config.DIFF_PATH = os.path.join(config.OUTPUT_PATH, 'diff')
+config.REPORT_PATH = os.path.join(config.OUTPUT_PATH, 'report')
+config.SOURCE_PATH = os.path.join(config.OUTPUT_PATH, 'sources')
+
 
 # Start.
 main()
