f4b0b5ddf4efcdf78f07f1564e3259cfd1128fe2
lmy375 <lmy375@139.com>
2024-04-01 20:42:41 +0800

    bump to 1.0.6

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ requires = filter(lambda x: not x.startswith("#"), requires)
 
 setup(
     name="peth",
-    description="An all-in-one Ethereum SDK and command-line tool, written in Python.",
+    description="An all-in-one Ethereum SDK and command-line tool written in Python.",
     url="https://github.com/lmy375/peth",
     author="Moon",
-    version="1.0.5",
-    packages=find_packages(exclude=["tests", "peth/4byte.json"]),
+    version="1.0.6",
+    packages=find_packages(exclude=["tests"]),
     python_requires=">=3.8",
     install_requires=requires,
     license="AGPL-3.0",
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     entry_points={
         "console_scripts": [
-            "peth = peth.__main__:main",
+            "peth = peth.cli:main",
             "tx-expl-server = peth.tools.txexpl.server.server:main",
         ]
     },
